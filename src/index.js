02cc73ef6d99aa0599a31fc59c91d2e7091ac386
Shivanshu Mishra <141329585+Shivanshu7007@users.noreply.github.com>
2024-01-09 02:34:26 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import dotenv from "dotenv";
-import connectDB from "./db/db.js";
-import {app} from './app.js'
-
+import dotenv from "dotenv"
+import connectDB from "./db/dataBase.js";
+import { app } from './app.js'
 dotenv.config({
     path: './.env'
 })
 
 connectDB()
-.then(()=>{
-    app.listen("Database Connected", process.env.PORT || 8000, () => {
-        console.log(`Server is Running${process.env.PORT}`)
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
     })
 })
-.catch((error)=>{
-    console.log("Error connecting to the database",error)
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
 })
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,5 +1,7 @@
-const asyncHandler = (requestHandler) => (req, res, next) =>{
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) =>{
     Promise.resolve(requestHandler(req,res,next)).catch(next);
+}
 }
 
 export { asyncHandler }
